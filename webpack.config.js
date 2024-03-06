850abe11e05a82a67aabfc97d3ba24836c89c92c
Andrii Hutnyk <andreigutnik@gmail.com>
2024-03-06 15:20:51 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,141 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const publicDir = path.resolve(__dirname, 'public');
 
-module.exports = (env) => {
-  const modules = {
-    js: {
+let mode = 'development';
+let target = 'web';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+  target = 'browserslist';
+}
+
+const devServer = {
+  historyApiFallback: true,
+  open: true,
+  compress: true,
+  allowedHosts: 'all',
+  hot: true,
+  static: path.resolve(__dirname, './src'),
+  port: 3000,
+};
+
+const plugins = [
+  new MiniCssExtractPlugin({
+    filename: '[name].[contenthash].css',
+  }),
+  new HtmlWebpackPlugin({
+    template: `${publicDir}/index.html`,
+  }),
+];
+
+const optimizations = {
+  splitChunks: {
+    cacheGroups: {
+      vendors: {
+        name: 'vendors',
+        test: /node_modules/,
+        chunks: 'all',
+        enforce: true,
+      },
+    },
+  },
+  minimizer: [],
+};
+
+const modules = {
+  rules: [
+    // --- Загрузчик для html (html-loader)
+    { test: /\.(html)$/, use: ['html-loader'] },
+    // --- Загрузчики стилей
+    {
+      test: /\.(s[ac]|c)ss$/i,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader',
+        'postcss-loader',
+      ],
+    },
+    {
+      test: /\.(js|mjs|jsx|ts|tsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    },
+    // --- Поддержка изображений
+    {
+      test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+      // В продакшен режиме
+      // изображения размером до 8кб будут инлайнится в код
+      // В режиме разработки все изображения будут помещаться в dist/assets
+      type: mode === 'production' ? 'asset' : 'asset/resource',
+    },
+    // --- Поддержка шрифтов
+    {
+      test: /\.(woff2?|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+    },
+    // --- Поддержка typescript
+    {
       test: /\.ts(x?)$/,
       exclude: /node_modules/,
-      use: [
-        {
-          loader: "ts-loader",
-        },
-      ],
+      loader: 'ts-loader',
     },
-    css: {
-      test: /\.css$/,
+    // --- Babel
+    {
+      test: /\.jsx?$/,
       exclude: /node_modules/,
       use: [
         {
-          loader: "style-loader",
+          loader: 'ts-loader',
         },
         {
-          loader: MiniCssExtractPlugin.loader,
-        },
-        {
-          loader: "css-loader",
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
         },
       ],
     },
-  };
+  ],
+};
 
-  if (env === "production") {
-    modules.stylus.use.splice(2, 0, { loader: "postcss-loader" });
-  }
+const resolve = {
+  extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  alias: {
+    App: path.resolve(__dirname, 'src'),
+    Components: path.resolve(__dirname, 'src/components'),
+  },
+};
 
-  const resolve = {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-    alias: {
-      App: path.resolve(__dirname, "src"),
-      Components: path.resolve(__dirname, "src/components"),
-    },
-  };
+module.exports = {
+  mode,
+  devServer,
+  target,
+  plugins,
+  devtool: 'source-map',
+  entry: {
+    main: './src/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext][query]',
+    clean: true,
+  },
+  // output: {
+  //   filename: watchMode
+  //     ? 'assets/[name].[hash].js'
+  //     : 'assets/[name].[chunkhash].js',
+  //   path: path.resolve(__dirname, 'build'),
+  //   publicPath: '/',
+  // },
 
-  return {
-    modules,
-    resolve,
-  };
+  resolve,
+  module: modules,
+
+  performance: {
+    hints: false,
+  },
+  optimization: optimizations,
+  stats: 'errors-only',
 };
